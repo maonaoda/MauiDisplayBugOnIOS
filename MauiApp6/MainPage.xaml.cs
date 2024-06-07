3ddc3f02f50c302188b4be3e7bd8a1a43124b7f9
maonaoda <maonaoda@gmail.com>
2024-06-06 22:31:41 -0700

    first push

---
+++ MauiApp6/MainPage.xaml.cs
@@ -0,0 +1,138 @@
+﻿#nullable disable
+using System.ComponentModel;
+using R3;
+
+namespace MauiApp6
+{
+    public class Option : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Name)));
+            }
+        }
+
+        private bool _isChecked;
+        public bool IsChecked
+        {
+            get => _isChecked;
+            set
+            {
+                _isChecked = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsChecked)));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Monkey
+    {
+        public BindableReactiveProperty<string> Name { get; }
+        public BindableReactiveProperty<int> Age { get; }
+
+        public BindableReactiveProperty<int> SelectIndex { get; }
+        public BindableReactiveProperty<bool> ShowOptions { get; }
+
+        public BindableReactiveProperty<List<Option>> Options { get; }
+
+        public Monkey(string name, int age, List<Option> options, int selectedIndex = -1)
+        {
+            Name = new BindableReactiveProperty<string>(name);
+            Age = new BindableReactiveProperty<int>(age);
+            SelectIndex = new BindableReactiveProperty<int>(selectedIndex);
+            ShowOptions = SelectIndex.Select(x => x == 1).ToBindableReactiveProperty();
+            Options = new BindableReactiveProperty<List<Option>>(options);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (Monkey)obj;
+            return Name == other.Name && Age == other.Age;
+        }
+
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ Age.GetHashCode();
+        }
+    }
+
+    public class MainPageViewModel
+    {
+        public BindableReactiveProperty<int> SelectIndex { get; }
+        public BindableReactiveProperty<bool> ShowOptions { get; }
+        public BindableReactiveProperty<List<Option>> Options { get; }
+
+        public BindableReactiveProperty<List<Monkey>> MonkeyList { get; }
+        public BindableReactiveProperty<List<Monkey>> MonkeyList2 { get; }
+
+        public MainPageViewModel()
+        {
+            MonkeyList = new BindableReactiveProperty<List<Monkey>>();
+            MonkeyList2 = new BindableReactiveProperty<List<Monkey>>();
+
+            SelectIndex = new BindableReactiveProperty<int>(-1);
+            ShowOptions = SelectIndex.Select(x => x == 1).ToBindableReactiveProperty();
+            Options = new BindableReactiveProperty<List<Option>>(new List<Option>
+            {
+                new Option { Name = $"A1" },
+                new Option { Name = $"A2" },
+                new Option { Name = $"A3" },
+            });
+        }
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var viewModel = new MainPageViewModel();
+            viewModel.MonkeyList.Value = new List<Monkey>
+            {
+                new Monkey("Tom",1, GetOptions(2)),
+                new Monkey("Kevi",2,GetOptions(2),1),
+                new Monkey("Jone1",3,GetOptions(2)),
+                new Monkey("Jone2",3,GetOptions(4)),
+                new Monkey("Jone3",3,GetOptions(6)),
+                new Monkey("Jone4",3,GetOptions(8)),
+            };
+
+            viewModel.MonkeyList2.Value = new List<Monkey>
+            {
+                new Monkey("Tom-1",1, GetOptions(2)),
+                new Monkey("Kevi-1",2,GetOptions(2),1),
+                new Monkey("Jone1-1",3,GetOptions(2)),
+                new Monkey("Jone2-1",3,GetOptions(4)),
+                new Monkey("Jone3-1",3,GetOptions(6)),
+                new Monkey("Jone4-1",3,GetOptions(8)),
+            };
+
+            BindingContext = viewModel;
+        }
+
+        private List<Option> GetOptions(int count)
+        {
+            var result = new List<Option>();
+            for (int i = 1; i <= count; i++)
+            {
+                result.Add(new Option { Name = $"A{i}" });
+            }
+
+            return result;
+        }
+    }
+}
---
+++ MauiApp6/MauiProgram.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+
+#nullable disable
+namespace MauiApp6
+{
+    public class CustomGrid : Grid { }
+
+    // temporary workaround for https://github.com/dotnet/maui/issues/17885
+    public class CustomGridHandler : LayoutHandler
+    {
+#if IOS
+        protected override CustomLayoutView CreatePlatformView()
+        {
+            return new CustomLayoutView()
+            {
+                CrossPlatformLayout = VirtualView
+            };
+        }
+
+        public class CustomLayoutView : Microsoft.Maui.Platform.LayoutView
+        {
+            double _lastMeasureHeight = double.NaN;
+            double _lastMeasureWidth = double.NaN;
+
+            public override void LayoutSubviews()
+            {
+                base.LayoutSubviews();
+
+                var bounds = AdjustForSafeArea(Bounds).ToRectangle();
+
+                var widthConstraint = bounds.Width;
+                var heightConstraint = bounds.Height;
+
+                var size = CrossPlatformLayout.CrossPlatformMeasure(widthConstraint, double.PositiveInfinity);
+                if (!(_lastMeasureHeight == size.Height && _lastMeasureWidth == size.Width)
+                    && CrossPlatformLayout is VisualElement element
+                    && Math.Abs(heightConstraint - size.Height) > 0.001)
+                {
+                    CacheMeasureConstraintsV2(size.Width, size.Height);
+                    element.InvalidateMeasureNonVirtual(Microsoft.Maui.Controls.Internals.InvalidationTrigger.Undefined);
+                }
+            }
+
+            protected void CacheMeasureConstraintsV2(double widthConstraint, double heightConstraint)
+            {
+                _lastMeasureWidth = widthConstraint;
+                _lastMeasureHeight = heightConstraint;
+            }
+        }
+#endif
+    }
+
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureMauiHandlers(h =>
+                {
+                    h.AddHandler(typeof(CustomGrid), typeof(CustomGridHandler));
+                })
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
